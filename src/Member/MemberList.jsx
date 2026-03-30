aee31a4d396540da2f352beefc2962fa654ee2dc
Noda Ryota <76949380+r-noda@users.noreply.github.com>
2026-03-30 13:15:31 +0900

    Update MemberList.jsx

--- src/Member/MemberList.jsx
+++ src/Member/MemberList.jsx
@@ -3,16 +3,29 @@ function MemberList({ members, isConfirmed, onRemove, onUpdate }) {
         <div id="members">
             {members.map((member) => (
                 <div className="member" key={member.id}>
-                    時給: 
+                    名前:
+                    {isConfirmed ? (
+                        <span className="memberNameText">{member.name}</span>
+                    ) : (
+                        <input
+                            id={`name-input-${member.id}`} 
+                            name={`name-${member.id}`} // name属性も追加するとより良い
+                            type="text" 
+                            className="memberNameInput"
+                            value={member.name || ""} // 名前なら .name ですね！
+                            onChange={(e) => onUpdate(member.id, "name", e.target.value)} // "name" を追加
+                        />
+                    )}
+                    時給:
                     {isConfirmed ? (
                         <span className="wageText">{member.wage}円</span>
                     ) : (
                         <>
-                            <input 
+                            <input
                                 type="number" 
                                 className="hourlyWageInput"
                                 value={member.wage || ""}
-                                onChange={(e) => onUpdate(member.id, e.target.value)}
+                                onChange={(e) => onUpdate(member.id, "wage", e.target.value)} // "wage" を追加
                             />
                             <button className="delete shadow" onClick={() => onRemove(member.id)}>削除</button>
                         </>
